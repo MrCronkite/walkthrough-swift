688138c87c03e83fb7157fb3a0c61978a8a86088
MrCronkite <vladshima99@gmail.com>
2022-05-04 21:50:29 +0300

    add new class

--- home_7.playground/Contents.swift
+++ home_7.playground/Contents.swift
@@ -2,3 +2,59 @@ import Foundation
 
 
 
+enum Weekday: String {
+    case monday = "1"
+    case tuesday = "aaaaaa"
+    case wednesday
+    case thuesday
+    case friday
+    case suterday
+    case sunday = "gfdsa"
+}
+
+
+var weekday: Weekday
+
+weekday = .sunday
+
+func setupAlarm(){
+    switch weekday{
+    case .sunday:
+        print("sunday")
+    case .suterday:
+        print("sunday")
+    case .friday:
+        print("sunday")
+    case .tuesday:
+        print("sunday")
+    case .thuesday:
+        print("sunday")
+    case .wednesday:
+        print("sunday")
+    case .monday:
+        print("sunday")
+    }
+}
+
+
+print(weekday)
+
+weekday = .friday
+
+print(weekday.rawValue)
+
+
+enum Planet: Int{
+    case mercury = 1, venus, earth = 6, mars, jupiter, saturn, uranus
+}
+
+var planet: Planet
+
+planet = .jupiter
+
+planet.rawValue
+
+
+if let pussebalNumber = Planet(rawValue: 9){
+    print(pussebalNumber)
+}
