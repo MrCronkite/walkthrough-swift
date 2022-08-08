d617742b4d1fb52a4a5f43a5910b63b39a8b0661
MrCronkite <vladshima99@gmail.com>
2022-08-08 21:25:17 +0300

    URLSession

---
+++ Networking/Networking/Course.swift
@@ -0,0 +1,21 @@
+//
+//  Course.swift
+//  Networking
+//
+//  Created by Alexey Efimov on 21/08/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+struct Course: Decodable {
+    let name: String?
+    let link: String?
+    let imageUrl: String?
+    let number_of_lessons: Int?
+    let number_of_tests: Int?
+}
+
+struct WebsiteDescription: Decodable {
+    let courses: [Course]?
+    let websiteDescription: String?
+    let websiteName: String?
+}
---
+++ Networking/Networking/ImageViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Alexey Efimov on 01/10/2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    private let imageUrl = "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg"
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImage()
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+    }
+
+    private func fetchImage() {
+        
+        guard let url = URL(string: imageUrl) else { return }
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
+}
+
