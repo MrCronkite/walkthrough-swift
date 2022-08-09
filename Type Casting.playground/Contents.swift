eac6fb303bc946cfbc2b81110fadc5d09d79a742
MrCronkite <vladshima99@gmail.com>
2022-08-09 16:31:14 +0300

    optional chaining

--- Type Casting.playground/Contents.swift
+++ Type Casting.playground/Contents.swift
@@ -121,3 +121,44 @@ for pet in pets {
         flightBirds(birds: bird)
     }
 }
+
+
+// опциональные цепочки
+
+class Address {
+    var street = "Deribasovskay"
+    var number = "1"
+    var city = "Odessa"
+    var country = "Ukraine"
+}
+
+struct Garage {
+    var size = 2
+}
+
+class House {
+    var rooms = 1
+    var address = Address()
+    var garage: Garage? = Garage()
+}
+
+class Car {
+    var model = "Zaporojec"
+    
+    func start() {
+    }
+}
+
+class Person {
+    var cars: [Car]? = [Car()]
+    var house: House? = House()
+}
+
+let p = Person()
+
+//p.cars![0]
+//p.house!
+
+//p.house!.garage!.size
+
+print(p.house?.garage?.size )
