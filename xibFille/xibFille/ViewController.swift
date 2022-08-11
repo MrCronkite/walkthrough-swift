1686b8a0ab7fe6051d2cfdc148bf0eb8a1589dbd
MrCronkite <vladshima99@gmail.com>
2022-08-11 21:23:05 +0300

    init progect

---
+++ xibFille/xibFille/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xibFille
+//
+//  Created by admin1 on 11.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
