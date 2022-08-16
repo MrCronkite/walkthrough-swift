f6ac2487f5bc9696c3c693473f272347684615b2
MrCronkite <vladshima99@gmail.com>
2022-08-16 22:29:36 +0300

    Error handling

--- error handling.playground/Contents.swift
+++ error handling.playground/Contents.swift
@@ -14,3 +14,61 @@ do {
 }
 
 let result = try? delay(a: 4, b: 2)
+
+
+//error Network
+
+class Network {
+    static let responses = [201, 401, 500]
+    
+    static func responsesDone() -> Int {
+        return responses.randomElement()!
+    }
+}
+
+enum ErrorRequest: Error {
+    case pageNotFound
+    case internalError(Int, String)
+}
+
+class NetworkMenage {
+    func userMenager(text: String) throws -> String {
+        let statuscode = Network.responsesDone()
+        
+        guard statuscode != 401 else { throw ErrorRequest.pageNotFound}
+        guard statuscode != 500 else { throw ErrorRequest.internalError(statuscode, "internet server error")}
+        
+        return "picture with \(text)"
+    }
+}
+
+class Browser {
+    
+    let networkManager: NetworkMenage
+    
+    init(networkManager: NetworkMenage){
+        self.networkManager = networkManager
+    }
+    
+    func getPage(request: String){
+        do {
+            let result = try networkManager.userMenager(text: request)
+            print(result)
+        } catch ErrorRequest.pageNotFound {
+            print("page not found")
+        } catch let ErrorRequest.internalError(code, reason) {
+            print("\(code) and \(reason)")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+let network = NetworkMenage()
+let chrome = Browser(networkManager: network)
+chrome.getPage(request: "Dogs")
+chrome.getPage(request: "Dogs")
+chrome.getPage(request: "Dogs")
+chrome.getPage(request: "Dogs")
+chrome.getPage(request: "Dogs")
+
