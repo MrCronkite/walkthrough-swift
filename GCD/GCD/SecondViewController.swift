e3056983d0c372ddcf613ba66a98a11fc6d1fdc8
MrCronkite <vladshima99@gmail.com>
2022-08-22 20:09:20 +0300

    get set

--- GCD/GCD/SecondViewController.swift
+++ GCD/GCD/SecondViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
