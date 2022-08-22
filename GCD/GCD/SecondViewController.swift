cd618b58294c99fcfd519f39f8f4e5ef4f5be434
MrCronkite <vladshima99@gmail.com>
2022-08-22 20:00:19 +0300

    gcd

---
+++ GCD/GCD/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  GCD
+//
+//  Created by admin1 on 22.08.22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ GCD/GCD/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by admin1 on 22.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
