397613f7c1b4045eddbf4bd964d0806182bd1efe
MrCronkite <vladshima99@gmail.com>
2023-04-13 22:47:38 +0300

    storage bg fix

---
+++ SubString.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+extension String {
+    func getRage(with position: Int) -> (startIndex: Int, endIndex: Int, range: Int) {
+        var char = Array(self)
+        var start = char.startIndex
+        var end = char.endIndex
+
+        if let index = char[position...].firstIndex(of: "\n") {
+            end = index - 1
+        }
+        if let index = char[0..<position].firstIndex(of: "\n") {
+            start = index + 1
+        }
+        return (start, end, end - start )
+    }
+}
+
+
+
+
+let someString: String = "test\ndfd1fd 2A  sdfdftest\n"
+
+
+print(someString.getRage(with: 16))
+print(someString.getRage(with: 1))
+print(someString.getRage(with: 4))
+print(someString.getRage(with: 17))
+
